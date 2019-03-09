f4065772eed31e0971c328572a9a657ab342729c
Jiachen Yan <jiacheny2@gmail.com>
2019-03-09 16:17:47 +1100

    Initial page set up and styling the page.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+
+const headerStyle = {
+  height: '100px'
+};
 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div style={headerStyle} className="bg-primary text-white d-flex justify-content-center align-items-center">
+          <h1>Loop Membership Management</h1>
+        </div>
+
+        <div id="singleMemberManagement" className="form-row p-3 d-flex justify-content-center">
+
+          <div className="col-md-2">
+            <label>Name</label>
+            <input className="form-control"/>
+          </div>
+
+
+          <div className="col-md-2">
+            <label>Type</label>
+            <select className="custom-select">
+              <option>Select type</option>
+              <option value="group">group</option>
+              <option value="user">user</option>
+            </select>
+          </div>
+
+          <div className="col-md-1 align-self-end">
+            <button className="btn btn-primary btn-block">
+              Create
+            </button>
+          </div>
+
+          <div className="col-md-1 align-self-end">
+            <button className="btn btn-primary btn-block">
+              Delete
+            </button>
+          </div>
+
+        </div>
+
+        <div id="memberAssignment" className="form-row p-3 d-flex justify-content-center">
+
+          <div className="col-md-2">
+            <label>Member</label>
+            <select className="custom-select">
+              <option>Select member</option>
+              <option disabled={true}>--- users ---</option>
+              <option disabled={true}>--- groups ---</option>
+            </select>
+          </div>
+
+
+          <div className="col-md-2">
+            <label>Group</label>
+            <select className="custom-select">
+              <option>Select group</option>
+            </select>
+          </div>
+
+          <div className="col-md-1 align-self-end">
+            <button className="btn btn-primary btn-block">
+              Add
+            </button>
+          </div>
+
+          <div className="col-md-1 align-self-end">
+            <button className="btn btn-primary btn-block">
+              Remove
+            </button>
+          </div>
+        </div>
+
+        <div id="membershipGraph">
+          <h3>Membership Graph</h3>
+          <div>Placeholders</div>
+        </div>
+
       </div>
+
     );
   }
 }
